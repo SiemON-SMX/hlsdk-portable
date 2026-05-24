1f366700ba4030b87d79ae90abfdc520a039548e
yalnie <232255780+yalnie@users.noreply.github.com>
2026-05-24 22:31:09 +0300

    test

--- android/app/src/main/java/su/xash/gunmod/MainActivity.java
+++ android/app/src/main/java/su/xash/gunmod/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,103 +15,127 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    private SharedPreferences prefs;
-    private static final String PREF_NAME = "prefs";
-    private static final String KEY_ARGV = "last_argv";
+	private SharedPreferences prefs;
+	private static final String PREF_NAME = "prefs";
+	private static final String KEY_ARGV = "last_argv";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        View root = findViewById(R.id.layout_launcher);
-        if (root != null) {
-            root.setFitsSystemWindows(true);
-            root.requestFocus();
-        }
+		View root = findViewById(R.id.layout_launcher);
+		if (root != null) {
+			root.setFitsSystemWindows(true);
+			root.requestFocus();
+		}
 
-        prefs = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+		prefs = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
 
-        EditText argvInput = findViewById(R.id.cmdArgs);
-        Button runButton = findViewById(R.id.button_launch);
+		EditText argvInput = findViewById(R.id.cmdArgs);
+		Button runButton = findViewById(R.id.button_launch);
 
-        argvInput.setText(prefs.getString(KEY_ARGV, "-console -log"));
-        argvInput.setSingleLine(true);
-        argvInput.setImeOptions(EditorInfo.IME_ACTION_DONE);
-        
-        argvInput.setOnEditorActionListener((v, actionId, event) -> {
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                saveArgs(argvInput.getText().toString());
-                argvInput.clearFocus();
-                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-                if (imm != null) imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                return true;
-            }
-            return false;
-        });
+		argvInput.setText(prefs.getString(KEY_ARGV, "-console -log"));
+		argvInput.setSingleLine(true);
+		argvInput.setImeOptions(EditorInfo.IME_ACTION_DONE);
+		
+		argvInput.setOnEditorActionListener((v, actionId, event) -> {
+			if (actionId == EditorInfo.IME_ACTION_DONE) {
+				saveArgs(argvInput.getText().toString());
+				argvInput.clearFocus();
+				InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_SERVICE);
+				if (imm != null) imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+				return true;
+			}
+			return false;
+		});
 
-        runButton.setOnClickListener(v -> {
-            String userArgs = argvInput.getText().toString();
-            saveArgs(userArgs);
-            startGame(userArgs.trim());
-        });
-    }
+		runButton.setOnClickListener(v -> {
+			String userArgs = argvInput.getText().toString();
+			saveArgs(userArgs);
+			startGame(userArgs.trim());
+		});
+	}
 
-    private void saveArgs(String args) {
-        prefs.edit().putString(KEY_ARGV, args).apply();
-    }
+	private void saveArgs(String args) {
+		prefs.edit().putString(KEY_ARGV, args).apply();
+	}
 
-    private void startGame(String argv) {
-        String pkg = "su.xash.engine.test";
-        try {
-            getPackageManager().getPackageInfo(pkg, 0);
-        } catch (Exception e) {
-            try {
-                pkg = "su.xash.engine";
-                getPackageManager().getPackageInfo(pkg, 0);
-            } catch (Exception ex) {
-                new AlertDialog.Builder(this)
-                    .setTitle(R.string.engine_not_found_title)
-                    .setMessage(R.string.engine_not_found_msg)
-                    .setPositiveButton(R.string.yes, (dialog, which) -> {
-                        startActivity(new Intent(Intent.ACTION_VIEW, 
-                            Uri.parse("https://github.com/FWGS/xash3d-fwgs/releases/tag/continuous")));
-                    })
-                    .setNegativeButton(R.string.no, null)
-                    .show();
-                return;
-            }
-        }
+	private void startGame(String argv) {
+		String pkg = "su.xash.engine.test";
+		try {
+			getPackageManager().getPackageInfo(pkg, 0);
+		} catch (Exception e) {
+			try {
+				pkg = "su.xash.engine";
+				getPackageManager().getPackageInfo(pkg, 0);
+			} catch (Exception ex) {
+				new AlertDialog.Builder(this)
+					.setTitle(R.string.engine_not_found_title)
+					.setMessage(R.string.engine_not_found_msg)
+					.setPositiveButton(R.string.yes, (dialog, which) -> {
+						startActivity(new Intent(Intent.ACTION_VIEW, 
+							Uri.parse("https://github.com/FWGS/xash3d-fwgs/releases/tag/continuous")));
+					})
+					.setNegativeButton(R.string.no, null)
+					.show();
+				return;
+			}
+		}
 
-        Intent intent = new Intent();
-        intent.setComponent(new ComponentName(pkg, "su.xash.engine.XashActivity"));
-        intent.putExtra("gamedir", "valve");
-        intent.putExtra("gamelibdir", getApplicationInfo().nativeLibraryDir);
-        intent.putExtra("argv", argv);
-        intent.putExtra("package", getPackageName());
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
+		Intent intent = new Intent();
+		intent.setComponent(new ComponentName(pkg, "su.xash.engine.XashActivity"));
+		intent.putExtra("gamedir", "valve");
+		intent.putExtra("gamelibdir", getApplicationInfo().nativeLibraryDir);
+		intent.putExtra("argv", argv);
+		intent.putExtra("package", getPackageName());
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem) {
-        int id = menuItem.getItemId();
-        if (id == R.id.action_github) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/SiemON-SMX/hlsdk-portable/tree/gunmod-updated")));
-            return true;
-        }
-        if (id == R.id.action_discord) {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://discord.gg/38YzeFrtvJ")));
-            return true;
-        }
-        return super.onOptionsItemSelected(menuItem);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem menuItem) {
+		int id = menuItem.getItemId();
+		if (id == R.id.action_github) {
+			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/SiemON-SMX/hlsdk-portable/tree/gunmod-updated")));
+			return true;
+		}
+		if (id == R.id.action_about) {
+			showAboutDialog();
+			return true;
+		}
+		if (id == R.id.action_discord) {
+			startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://discord.gg/38YzeFrtvJ")));
+			return true;
+		}
+		return super.onOptionsItemSelected(menuItem);
+	}
+
+	private void showAboutDialog() {
+		AlertDialog dialog = new AlertDialog.Builder(this)
+				.setTitle(R.string.about)
+				.setMessage(R.string.about_msg) 
+				.setPositiveButton(R.string.ok, null) 
+				.setNegativeButton(R.string.download_pk3, (d, which) -> {
+					startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://raw.githubusercontent.com/SiemON-SMX/hlsdk-portable/refs/heads/gunmod-updated/pak/gunmod.pk3")));
+				})
+				.create();
+
+		dialog.show();
+
+		TextView messageView = (TextView) dialog.findViewById(android.R.id.message);
+		if (messageView != null) {
+			messageView.setAutoLinkMask(Linkify.WEB_URLS);
+			messageView.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
+		}
+	}
 }
